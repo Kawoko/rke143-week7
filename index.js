edf9a3364a8bb1f2ccb9175e606a4a21d430ba8c
0noca <rasmusvaravas@gmail.com>
2026-01-29 16:09:38 +0200

    cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 //Routes
 const rkeRouter = require('./routes/rke143');
@@ -11,6 +12,9 @@ const randomRouter = require('./routes/random');
 const path = require('path');
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
